f222c8fa19c4454da79fc99b262bbf65b92ba6ba
Seung-Sup Lee <sslee@snu.ac.kr>
2022-09-23 21:09:00 +0900

    Upload T08.1 exercises

---
+++ Tutorials/T08.1_DMRG_GroundState_1site/DMRG_GS_1site_Ex.m
@@ -0,0 +1,237 @@
+function [M,E0,Eiter,Sv] = DMRG_GS_1site_Ex (M,Hs,Nkeep,Nsweep,varargin)
+% < Description >
+%
+% [M,E0,Eiter,Sv] = DMRG_GS_1site_Ex (M,Hs,Nkeep,Nsweep [,'Krylov',nKrylov] [,'tol',tol])
+%
+% Single-site density-matrix renormalization group (DMRG) calculation to
+% search for the ground state and its energy of a one-dimensional system,
+% whose Hamiltonian is given by the matrix product operator.
+%
+% < Input >
+% M : [1 x N cell array] Matrix product state (MPS) as the initial guess of
+%       the variational search. Each M{n} is a rank-3 tensor acting on site
+%       n. Their legs are ordered as left-right-bottom(=physical). The
+%       length M, i.e., numel(Hs), defines the system length N.
+% Hs : [1 x N cell array] Matrix product operator (MPO) of the Hamiltonian.
+%       Each Hs{n} is a rank-4 tensor acting on site n. The order of legs
+%       of Hs{n} is bottom-top-left-right, where the bottom (top) leg
+%       contracts to the physical leg of bra (ket) tensor. 
+% Nkeep : [numeric] Maximum bond dimension of the matrix product state
+%       (MPS) to keep.
+% Nsweep : [numeric] Number of round trips of sweeping. There will be
+%       Nsweep pairs of right-to-left sweep and left-to-right sweep. That
+%       is, the first sweep is right-to-left and the last is left-to-right.
+%
+% < Option >
+% 'Krylov', .. : [numeric] The maximum dimension of the Krylov subspace to
+%       be considered, within the Lanczos method used for updating each MPS
+%       tensor. That is, a tridiagonal matrix of size n-by-n (at maximal)
+%       is diagonalized in each tensor update, with n set by 'Krylov',.. .
+%       (Default: 5)
+% 'tol', .. : [numeric] The tolerance for elements on the +-1 diagonals
+%       (those next to the main diagonal) within the Lanczos method. If an
+%       element is smaller than the tolerance, then the Lanczos
+%       tridiagonalization procedure stops and only the part of the
+%       tridiagonal matrix constructed so far is diagonalized.
+%       (Default: 1e-8)
+%
+% < Output >
+% M : [1 x N cell array] The result MPS which is obtained variationally to
+%       have the minimum expectation value of the Hamiltonian Hs. It is in
+%       a left-canonical form, since the last sweep is left-to-right.
+% E0 : [numeric] The energy of M.
+% Eiter : [N x (2*Nsweep) numeric array] Each element Eiter(m,n) means the
+%       variational energy in the m-th iteration within the n-th sweep.
+%       Odd n is for right-to-left sweep and even n for left-to-right
+%       sweep. Note that the iteration index m matches with the site index
+%       for left-to-right sweep; the iteration m corresponds to the site
+%       (N+1-m) for right-to-left sweep.
+% Sv : [1 x (N+1) cell array] Sv{n} contains a vector of singular values on
+%       the bond between sites n-1 and n, for 1 < n < N. Sv{1} and Sv{end}
+%       are the norms of the MPS, sitting on the left- and rightmost legs
+%       (that are dummy), respectively.
+%
+% Written by S.Lee (May 28,2019)
+% Updated by S.Lee (May 23,2020): Revised for SoSe2020.
+% Rewritten by S.Lee (Sep.23,2022) for the 2022 fall semester at SNU.
+
+
+tobj = tic2;
+
+% default vales of optional input parameters
+nKrylov = 5;
+tol = 1e-8;
+
+% parse options
+while ~isempty(varargin)
+    if numel(varargin) < 2
+        error('ERR: Option should be set by a pair of option name and value.');
+    end
+    switch varargin{1}
+        case 'Krylov'
+            nKrylov = varargin{2};
+            varargin(1:2) = 0;
+        case 'tol'
+            tol = varargin{2};
+            varargin(1:2) = 0;
+        otherwise
+            error('ERR: Unknown input.');
+    end
+end
+
+% % sanity check for input and option
+N = numel(M);
+if N < 2
+    error('ERR: chain is too short.');
+elseif N ~= numel(Hs)
+    error('ERR: The lengths of ''M'' and ''Hs'' should be equal.');
+end
+
+for itN = (1:N)
+    if ~all(size(M{itN},3) == [size(Hs{itN},1), size(Hs{itN},2)])
+        error(['ERR: The first or second leg of Hs{',sprintf('%i',itN), ...
+            '} has dimension inconsistent with the physical leg of an MPS tensor.']);
+    end
+end
+% % %
+
+% show message
+disptime('Single-site DMRG: ground state search');
+disptime(['# of sites = ',sprintf('%i',numel(Hs)), ...
+    ', Nkeep = ',sprintf('%i',Nkeep),', # of sweeps = ',sprintf('%i',Nsweep),' x 2']);
+
+% bring into a left-canonical form without extra truncation
+M = canonForm(M,N,[],0);
+
+% ground-state energy for each iteration
+Eiter = zeros(N,2*Nsweep);
+% later, Eiter(end,end) will be taken as the final result E0
+
+Sv = cell(1,N); % collection of singular value vectors
+
+% Contractions of MPO and MPS tensors that represent the effective
+% Hamiltonian for the left/right parts of the chain: When the orthogonality
+% center (= central tensor in a site-canonical form) is at site n, then
+% Hlr{m+1} for m < n (m > n) is the left (right) part of the effective
+% Hamiltonian made of M(1:m) and Hs(1:m) [M(m:end) and Hs(m:end)].
+Hlr = cell(1,N+2);
+Hlr{1} = 1; % to initialize "zipper" contraction
+Hlr{end} = 1; % to represent the Hamiltonian for the dummy space
+
+% Since M is in left-canonical form by now, we build Hlr{..} by contracting
+% left-to-right.
+for itN = (1:N)
+    Hlr{itN+1} = updateLeft(Hlr{itN},3,M{itN},Hs{itN},4,M{itN});
+end
+
+for itS = (1:Nsweep)
+    % right -> left
+    for itN = (N:-1:1)
+        % % % % TODO (start) % % % %
+        % Use the subfunction eigs_1site_GS, put at the end of this file,
+        % to obtain the ground state via the Lanczos method
+        [M{itN},Eiter(N+1-itN,2*itS-1)] = eigs_1site_GS (Hlr{itN},Hs{itN},Hlr{itN+2},M{itN},nKrylov,tol);
+
+        % SVD, using Skeep = 0 not to decrease bond dimensions
+        
+        
+        % update the next tensor
+        
+
+        % update the Hamiltonian in effective basis
+        
+
+        % % % % TODO (end) % % % %
+    end
+    
+    % display information of the sweep
+    disptime(['Sweep #',sprintf('%i/%i',[2*itS-1,2*Nsweep]),' (right -> left) : Energy = ', ...
+        sprintf('%.7g',Eiter(N,2*itS-1))]);
+    
+    % left -> right
+    for itN = (1:N)
+        % % % % TODO (start) % % % %
+        [M{itN},Eiter(itN,2*itS)] = eigs_1site_GS (Hlr{itN},Hs{itN},Hlr{itN+2},M{itN},nKrylov,tol);
+        
+        % SVD, using Skeep = 0 not to decrease bond dimensions
+        
+
+        % update the next tensor
+        
+
+        % update the Hamiltonian in effective basis
+        
+
+        % % % % TODO (end) % % % %
+    end
+
+    % display informaiton of the sweep
+    disptime(['Sweep #',sprintf('%i/%i',[2*itS,2*Nsweep]),' (left -> right) : Energy = ', ...
+        sprintf('%.7g',Eiter(N,2*itS))]);
+end
+
+E0 = Eiter(N,2*itS); % take the last value
+    
+toc2(tobj,'-v');
+
+end
+
+function [Anew,Enew] = eigs_1site_GS (Hleft,Hcen,Hright,Aold,nKrylov,tol)
+% < Description >
+%
+% Anew = eigs_1site_GS (Hleft,Hcen,Hright,Aold)
+%
+% Update an MPS tensor acting on a single site, by solving the effective
+% Hamiltonian via the Lanczos method.
+%
+% < Input >
+% Hleft : [rank-3 tensor] Left part of the effective Hamiltonian. Its legs
+%       are ordered as bottom-top-right.
+% Hcen : [rank-4 tensor] Center part of the effective Hamiltonian. It is
+%       indeed an MPO tensor for the current site.
+% Hright : [rank-3 tensor] Right part of the effective Hamiltonian. Its
+%       legs are ordered as bottom-top-left.
+% Aold : [rank-3 tensor] Current ket tensor.
+% nKrylov, tol : Options for the Lacnzos method. See the documentation for
+%       the parent function for details.
+%
+% < Output >
+% Anew : [rank-3 tensor] The approximate ground state of the effective
+%       Hamiltonian obtained by the Lanczos method, using the Krylov
+%       subspace with dimension up to "nKrylov".
+% Enew : [numeric] Expectation value of the effective Hamiltonian with
+%       respect to "Anew".
+
+% The collection of rank-3 tensors as Krylov basis; the 4th dimension is
+% for indexing different rank-3 tensors
+As = zeros([size(Aold,1),size(Aold,2),size(Aold,3),nKrylov]);
+
+% Define the first Krylov vector as the input "Aold"
+As(:,:,:,1) = Aold/sqrt(abs(contract(conj(Aold),3,(1:3),Aold,3,(1:3))));
+% normalize; insert "abs" to avoid numerical noise
+
+alphas = zeros(nKrylov,1); % main diagonal elements
+betas  = zeros(nKrylov-1,1); % +-1 diagonal elements
+cnt = 0; % counter for Krylov subspace dimension
+
+for itn = (1:nKrylov)
+    % % % % TODO (start) % % % %
+    
+
+    % % % % TODO (end) % % % %
+end
+
+Hkrylov = diag(betas(1:cnt-1),-1);
+Hkrylov = Hkrylov + Hkrylov' + diag(alphas(1:cnt));
+
+[V,D] = eig(Hkrylov);
+[~,minid] = min(diag(D));
+Anew = contract(As(:,:,:,1:cnt),4,4,V(:,minid),2,1);
+
+% compute the epectation value of the effective Hamiltonian with respect to "Anew"
+Amul = contract(Hleft,3,2,Anew,3,1);
+Amul = contract(Amul,4,[2 4],Hcen,4,[3 2]);
+Amul = contract(Amul,4,[2 4],Hright,3,[2 3],[1 3 2]);
+Enew = real(contract(conj(Anew),3,(1:3),Amul,3,(1:3)));
+
+end
---
+++ Tutorials/T08.1_DMRG_GroundState_1site/DMRG_GroundState_1site.m
@@ -0,0 +1,155 @@
+%% DMRG: Single-site update for ground state search
+% Author: <https://cqm.snu.ac.kr Seung-Sup Lee>
+%% 
+% In this tutorial, we will obtain the ground state of one-dimensional systems 
+% by using the *density-matrix renormalization group (DMRG)*. 
+%% XY spin chain: ground state search
+% The first example to solve is the XY spin chain, where spin-1/2's lie along 
+% a one-dimensional lattice of length $L$ and the spins have nearest-neighbor 
+% interaction. The name "XY" comes from that only the $x$ and $y$ components of 
+% the neighboring spins are coupled:
+% 
+% $$\hat{H}_{\mathrm{XY}}= -\sum_{\ell=1}^{L-1} (\hat{S}_{\ell,x} \hat{S}_{\ell+1,x} 
+% + \hat{S}_{\ell,y} \hat{S}_{\ell+1,y})= -\frac{1}{2} \sum_{\ell=1}^{L-1} (\hat{S}_{\ell,+} 
+% \hat{S}_{\ell+1,-} + \hat{S}_{\ell,-} \hat{S}_{\ell+1,+}) .$$
+% 
+% By using the <https://en.wikipedia.org/wiki/Jordan%E2%80%93Wigner_transformation 
+% Jordan-Wigner transformation>, the XY model is mapped onto a non-interacting 
+% tight-binding chain of spinless fermions. So the model is exactly solvable, 
+% though we numerically diagonalized the single-particle Hamiltonians in the previous 
+% exercises. As shown in those exercises, the tight-binding chain show different 
+% behaviors (e.g., ground-state degeneracy) depending on the parity of $L$. Here 
+% we focus on even $L$, where the ground-state energy is
+% 
+% $$E_{0} = \frac12 - \frac{1}{2\sin \frac{\pi}{2(N+1)}} ,$$
+% 
+% and the correlation function of nearest-neighbor spins with respect to the 
+% ground state is
+% 
+% $$\langle \Psi_0 |\hat{S}^+_\ell \hat{S}^-_{\ell+1} | \Psi_0 \rangle = \langle 
+% \Psi_0 | \hat{S}^-_\ell \hat{S}^+_{\ell+1} | \Psi_0 \rangle = -\frac{1}{2(N+1)} 
+% \left[  \frac{(-1)^\ell}{\sin\frac{(2\ell+1)\pi}{2(N+1)}} - \frac{1}{\sin\frac{\pi}{2(N 
+% +1)}} \right] ,$$
+% 
+% where $| \Psi_0 \rangle$ means the ground state of the XY model. 
+% 
+% We will implement the single-site DMRG method and compare the DMRG result 
+% with the exact one. Let's first set the parameters for the Hamiltonian and the 
+% DMRG routine.
+
+clear
+
+% system parameter
+J = -1; % coupling strength
+L = 40; % number of sites in a chain
+
+% DMRG parameter
+Nkeep = 30; % bond dimension
+Nsweep = 4; % number of pairs of left+right sweeps
+
+% Local operators
+[S,I] = getLocalSpace('Spin',1/2);
+%% 
+% Now we construct the bulk tensor for the MPO representation of the XY spin 
+% chain Hamiltonian. The tensor comprises the $L-2$ tensors of the MPO, except 
+% for the first and last tensors on the boundaries; hence bulk.
+
+% % MPO formulation of Hamiltonian
+% Hamiltonian tensor for each chain site
+Hloc = cell(4,4);
+Hloc(:) = {zeros(size(I))};
+Hloc{1,1} = I;
+Hloc{2,1} = S(:,:,1);
+Hloc{3,1} = S(:,:,3);
+Hloc{4,2} = J*S(:,:,1)';
+Hloc{4,3} = J*S(:,:,3)';
+Hloc{end,end} = I;
+Hloc = cell2mat(reshape(Hloc,[1 1 size(Hloc,1) size(Hloc,2)]));
+
+% full chain
+Hs = cell(1,L);
+Hs(:) = {Hloc};
+Hs{1} = Hs{1}(:,:,end,:); % choose the last components of the left leg
+Hs{end} = Hs{end}(:,:,:,1); % choose the first components of the right leg
+%% Exercise (a): Initialize MPS with the iterative diagonalization result
+% Before starting the iterative ground state search, we need to specify an MPS 
+% as the starting point. For this, one can use either the iterative diagonalization 
+% result or random tensors. Write the code that performs *iterative diagonalization 
+% with the MPO Hamiltonian.* At each iteration, keep at most |Nkeep| states, *without* 
+% keeping degenerate states near truncation thresholds altogether (or discard 
+% them altogether). Obtain an MPS represented by a cell array |M|, where |M{n}| 
+% is a rank-3 tensor for site n.
+%% Exercise (b): Complete the single-site DMRG function
+% There is a function |DMRG_GS_1site_Ex.m|, which is in the same sub-directory 
+% with this function. It is incomplete. Complete the parts enclosed by the comments 
+% |TODO (start)| and |TODO (end)|.
+%% 
+% You can verify your solutions to Exercises (a) and (b) by comparing them with 
+% the exact results. Upon successful implementation, the |DMRG_1site_Ex| function 
+% will run like:
+
+[M0,E0,Eiter] = DMRG_GS_1site(M,Hs,Nkeep,Nsweep);
+%% 
+% Compare the DMRG result of the ground-state energy, |E0|, with the exact value.
+
+E0_exact = 0.5 - (1/2/sin(pi/2/(L+1))); % exact value
+disptime(['Exact GS energy = ',sprintf('%.5g',E0_exact),', DMRG = ', ...
+    sprintf('%.5g',E0),', error = ',sprintf('%.5g',E0-E0_exact)]);
+%% 
+% Plot how the expectation value of the Hamiltonian converges to the exact value 
+% as iteration proceeds.
+
+figure;
+semilogy((1:numel(Eiter))/L,Eiter(:)-E0_exact,'LineWidth',1);
+set(gca,'FontSize',13,'LineWidth',1);
+xlim([0 2*Nsweep]);
+grid on;
+xlabel('# of sweeps');
+ylabel('Ground-state energy error');
+%% 
+% The error is $O(10^{-6})$. The error can be further decreased by increasing 
+% bond dimension |Nkeep|. Indeed, the accuracy is bound by finite bond dimension, 
+% since the system is gapless. Consider a bipartition of the chain, into left 
+% and right halves. The entanglement between two parts in the ground state increases 
+% with the system size $L$ logarithmically, for the gapless systems. Thus, to 
+% achieve the same accuracy, we should use larger bond dimension that increases 
+% as the power law of $L$.
+% 
+% On the other hand, for the gapped systems, high accuracy can be achieved with 
+% small bond dimensions. For example, the AKLT model needs only the bond dimension 
+% 2.
+%% Spin-spin correlation function
+% Compute the spin-spin correlation function $\langle \Psi_0 | \hat{S}_{\ell,+} 
+% \hat{S}_{\ell+1,-} | \Psi_0 \rangle$, i.e., the expectation value of the product 
+% of spin-raising and -lowering operators at nearest-neighbor sites with respect 
+% to the ground state $|\Psi_0\rangle$. Be aware of that the result MPS |M| is 
+% in a left-canonical form, as mentioned in the documentation of |DMRG_GS_1site_Ex.m|.
+
+% compute correlation function for the nearest-neighbour spins
+SS = zeros(1,L-1);
+for itN = (2:L)
+    T = updateLeft([],[],M0{itN-1},S(:,:,1)*sqrt(2),3,M0{itN-1});
+    T = updateLeft(T,3,M0{itN},S(:,:,3)*sqrt(2),3,M0{itN});
+    
+    for itN2 = ((itN+1):L)
+        T = updateLeft(T,2,M0{itN2},[],[],M0{itN2});
+    end
+    
+    SS(itN-1) = T;
+end
+
+% exact relation
+SS_exact = (((-1).^(1:L-1))./sin((2*(1:L-1)+1)*pi/2/(L+1)) - ...
+    1./sin(pi/2/(L+1)))/(-2*(L+1));
+
+figure;
+plot((1:L-1),SS,'-',(1:L-1),SS_exact,'--','LineWidth',1);
+legend({'DMRG','Exact'});
+set(gca,'FontSize',13,'LineWidth',1);
+xlabel('$\ell$','Interpreter','latex');
+ylabel('$\langle \hat{S}_{\ell,+} \hat{S}_{\ell+1,-} \rangle$', ...
+    'Interpreter','latex');
+xlim([1 L-1]);
+grid on;
+%% 
+% The numerical result and the exact result coincide nicely!
