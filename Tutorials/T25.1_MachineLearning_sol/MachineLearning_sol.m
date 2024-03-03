6c28d12fee9e0b9b93272abb938878e63238f395
Seung-Sup Lee <sslee@snu.ac.kr>
2024-03-03 14:13:17 +0900

    Upload the T25 solution (which I forgot to upload for 1.5 years...)

---
+++ Tutorials/T25.1_MachineLearning_sol/MachineLearning_sol.m
@@ -0,0 +1,8 @@
+%% [Solution] Machine learning with MPS
+% Author: <https://cqm.snu.ac.kr Seung-Sup Lee>
+%% Solution to Exercise (a): Complete the function for the MPS-based machine learning method
+% The complete version is uploaded as |ML/ML_MPS.m|. Compare with your implementation!
+%% 
+% This is the last tutorial exercise throughout this lecture course. If you 
+% have found any bugs, mistakes, typos, etc. from the tutorial materials so far, 
+% please contact us. Thank you for your participation!
